639331105479f68535f5a983a426ba7f4735d294
guilherme.ns <guilherme.silva.ex@synchro.com.br>
2023-08-22 14:06:26 -0300

    refactoring errors in UserController

--- api/src/main/java/com/guigasauro/teamdraftmanagementsys/api/UserController.java
+++ api/src/main/java/com/guigasauro/teamdraftmanagementsys/api/UserController.java
@@ -1,21 +1,11 @@
 package com.guigasauro.teamdraftmanagementsys.api;
 
-import com.guigasauro.teamdraftmanagementsys.domain.User;
-import com.guigasauro.teamdraftmanagementsys.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
-
-    private UserRepository repository;
-    @GetMapping
-    public void getAll(){
-        List<User> userList =
-
-    }
 }
